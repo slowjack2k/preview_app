d90746a87f7bc118a9413461dee5f821045d1bf1
Dieter Spaeth <dieter.spaeth@gmx.de>
2013-08-20 19:25:19 +0200

    barebone rails 3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PreviewApp::Application.config.secret_token = '30b8ae5af4e8442aaee85d29559ae2e8355bb56cc0d62c1e175d9b33c773e0e5c7b0977fb7d42782d8f7777413e6d41bef1839c61ef40a3ad3102b51098b7830'
