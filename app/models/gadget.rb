41b06f5455d09e29473db75e710c4709c4ef9619
Dieter Spaeth <dieter.spaeth@gmx.de>
2013-08-22 20:35:33 +0200

    added uploader

--- app/models/gadget.rb
+++ app/models/gadget.rb
@@ -1,3 +1,4 @@
 class Gadget < ActiveRecord::Base
   belongs_to :user
+  mount_uploader :gadget_picture, GadgetPictureUploader
 end
--- db/migrate/20130822163202_create_gadgets.rb
+++ db/migrate/20130822163202_create_gadgets.rb
@@ -2,6 +2,7 @@ class CreateGadgets < ActiveRecord::Migration
   def change
     create_table :gadgets do |t|
       t.string :name
+      t.string :gadget_picture
       t.integer :user_id
       t.timestamps
     end
