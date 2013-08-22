feb4763e93dd71dc0de6c9334c677e18dcef725c
Dieter Spaeth <dieter.spaeth@gmx.de>
2013-08-22 18:52:17 +0200

    added login attribute

--- db/migrate/20130822162956_create_users.rb
+++ db/migrate/20130822162956_create_users.rb
@@ -1,8 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string :login
       t.timestamps
     end
+
+    add_index(:users, :login)
   end
 end
