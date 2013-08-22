ff1996ac823f9fc556bf303620ef2d60b1cf11ff
Dieter Spaeth <dieter.spaeth@gmx.de>
2013-08-22 18:47:22 +0200

    added test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
 describe User do
-  it "should have many gadgets" do
+  it "should own many gadgets" do
     t = User.reflect_on_association(:gadgets)
     t.macro.should == :has_many
   end
+
+  [:login].each do |attribute|
+     it "should have attribute #{attribute}" do
+       user = User.new
+       user.should respond_to attribute
+     end
+  end
+
 end
